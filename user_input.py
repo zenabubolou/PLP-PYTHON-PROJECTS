ec97ed9ec1c5635da47c7af9630a0d860952cb7d
zenabu bolou <zenabobolou@gmail.com>
2024-02-23 22:58:25 +0100

    python week 1 assignment Changes to be committed: new file:   user_input.py

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+age = input("What is your age: ")
+location = input("What is your location: ")
+print(f"Hello {name} you are {age}yrs old and live in {location}")
