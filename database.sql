23fa983c0260ddf4d456810230406725c558d616
zenabu bolou <zenabobolou@gmail.com>
2024-02-27 10:23:53 +0100

    database week1 assignment new file:   database.sql

---
+++ database.sql
@@ -0,0 +1,62 @@
+Week 1 Assignment: Database Design - Data vs. Information
+This form has been created to collect submissions for the week 1 assignment.
+zenabubolou@gmail.com Switch account
+ 
+Not shared
+ 
+* Indicates required question
+Email: 
+*
+zenabubolou@gmail.com
+First Name:
+*
+zenabu
+Last Name: 
+*
+bolou
+Section A: Definitions
+
+1.    
+Define the following key terms related to databases:
+
+1.1.1.     Database
+
+It is an organized collection of data, so that it can be easily accessed and managed.
+1.1.2.     Table
+
+It is a collection of related data held in a table format within a database. It consists of columns and rows.
+1.1.3.     Record
+
+is a set of data stored in a table. example a customer record.
+1.1.4.     Field
+
+Is an attribute of a record in a database table
+1.1.5.     Primary Key
+
+Is the columns or column that contains values that uniquely identify each row in a table
+1.1.6.     SQL
+
+Is a programming language for storing and processing data in a relational database.
+1.1.7.     Query
+
+ is a request for information from the  database management system
+1.1.8.     Index
+
+ is a copy of selected columns of data, from a table, that is designed to enable very efficient search.
+1.1.9.     Normalization
+
+ is the process of organizing data in a database. It includes creating tables and establishing relationships between those tables according to rules designed both to protect the data and to make the database more flexible by eliminating redundancy and inconsistent dependency.
+1.1.10.  Database Management System (DBMS)
+It serves as an interface between an end-user and a database. it is a software that allows users to  create, read, update, and delete data in the database.
+2.  Section B: Discussions
+1.1.1.     Describe the purpose of a primary key in a database table and provide an example.
+
+The primary key is a unique identifier for rows in a table, so that no two records in the table have the same value. This means that if you have a Customer table, all Customer will have a different CustomerId. A primary key uniquely identifies each row.
+1.1.2.     Explain the difference between a database management system (DBMS) and a database.
+
+database is a collection of data
+dbms is a software that helps manage the database. it includes how data are stored, accessed, updated, and deleted from the database.
+1.1.3.     Discuss in short, the importance of normalization in database design and provide an example of how it can improve data integrity.
+
+ It eliminates redundancy, minimize data modification errors, and simplify the query process.
+By eliminating redundancy, it ensures that updates, inserts, and deletes are performed consistently across the database
